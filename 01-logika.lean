98c3eae4278a6b3394bf0bb4d7ab42609fa53bda
Ivan Bokor <ivanbokor17@gmail.com>
2023-05-11 19:10:36 +0000

    solved first 4 problems

--- 01-logika.lean
+++ 01-logika.lean
@@ -1,14 +1,62 @@
+#print Or
+
 example : p ∧ q ↔ q ∧ p :=
-  sorry
+  have ltr : p ∧ q → q ∧ p :=
+    λ hpq : p ∧ q =>
+      have hp : p := And.left hpq
+      have hq : q := And.right hpq
+      And.intro hq hp
+  have rtl : q ∧ p → p ∧ q :=
+    λ hqp : q ∧ p => 
+      have hp : p := And.right hqp
+      have hq : q := And.left hqp
+      And.intro hp hq
+  Iff.intro ltr rtl
 
 example : p ∨ q ↔ q ∨ p :=
-  sorry
+  have ltr : p ∨ q → q ∨ p :=
+    λ hpq : p ∨ q => by
+      cases hpq with
+      | inl hp => exact Or.inr hp 
+      | inr hq => exact Or.inl hq
+  have rtl : q ∨ p → p ∨ q :=
+    λ hqp : q ∨ p => by
+      cases hqp with
+      | inr hp => exact Or.inl hp
+      | inl hq => exact Or.inr hq
+  Iff.intro ltr rtl
+      
 
-example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) :=
-  sorry
+example : (p ∧ q) ∧ r ↔ p ∧ (q ∧ r) := by
+  have ltr : (p ∧ q) ∧ r → p ∧ (q ∧ r) := by
+    intro hpqr
+    have hpq := hpqr.left 
+    have hp := hpq.left
+    have hq := hpq.right
+    have hr := hpqr.right
+    apply And.intro hp (And.intro hq hr)
+  have rtl : p ∧ (q ∧ r) → (p ∧ q) ∧ r := by
+    intro hpqr
+    have hqr := hpqr.right
+    have hp := hpqr.left
+    have hq := hqr.left
+    have hr := hqr.right
+    apply And.intro (And.intro hp hq) hr
+  apply Iff.intro ltr rtl
   
-example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) :=
-  sorry
+example : (p ∨ q) ∨ r ↔ p ∨ (q ∨ r) := by
+  apply Iff.intro
+  
+  . intro inl 
+    cases inl with
+    | inr hr => exact Or.inr (Or.inr hr)
+    | inl hpq => 
+      cases hpq with
+      | inl hp => exact Or.inl (Or.inl hp)
+      | inr hq => 
+  . intro inr
+    sorry
+  
 
 example : p ∧ (q ∨ r) ↔ (p ∧ q) ∨ (p ∧ r) :=
   sorry
